06352e95626d5eeab98dc3214a34feffd68c89d2
Hady Ahmed <h.a7med2017@gmail.com>
2019-04-24 00:50:24 +0200

    Capstone stage - 02 v1.4

--- app/src/main/java/com/hadi/bc/CardsAppWidget.java
+++ app/src/main/java/com/hadi/bc/CardsAppWidget.java
@@ -1,13 +1,9 @@
 package com.hadi.bc;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
 import android.widget.RemoteViews;
-
-import com.hadi.bc.view.AddCardActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -19,10 +15,8 @@ public class CardsAppWidget extends AppWidgetProvider {
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cards_app_widget);
-        Intent intent = new Intent(context, AddCardActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        views.setOnClickPendingIntent(R.id.addCardBtn, pendingIntent);
+        views.setTextViewText(R.id.textView, "test widget text view");
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -34,16 +28,6 @@ public class CardsAppWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
